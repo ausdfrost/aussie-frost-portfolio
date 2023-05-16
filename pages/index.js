8516075d0945e5007f1721808a7ac5a77e31e8c2
Aussie Frost <65328557+austinfroste@users.noreply.github.com>
2023-05-16 10:21:04 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,18 +6,45 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>@austinfroste | Aussie Frost</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+      <Header />
 
-      <Footer />
+      <body>
+	      <div>
+		      <p>Howdy, I'm <b>Aussie Frost</b>, a third year data science student working in UO's Computational Social Neuroscience Lab. 
+          If you have any inquiries, or would just like to chat, feel free to email! You can also check out my projects on GitHub. ✌️</p>
+    	    <a href="mailto:afrost6@uoregon.edu">Email</a> <a href="https://github.com/austinfroste">GitHub</a> <a href="https://csnl.uoregon.edu/people/">CSNLab</a>
+	      </div>
+
+        <hr></hr>
+
+        <div>
+          <h2>2022</h2>
+          <div>
+            <a classname="blog-post-title" href="/2022/08/18/bids-conversion">BIDS Conversion</a>
+            <span className="blog-post-date">18 August</span>
+          </div>
+          <div>
+            <a classname="blog-post-title" href="/2022/04/24/gpa-calcuator">GPA Calculator</a>
+            <span className="blog-post-date">24 April</span>
+          </div>
+
+          <h2>2021</h2>
+            <div>
+              <a classname="blog-post-title" href="/2021/04/16/dungeon-crawler">CS 162 Dungeon Crawler</a>
+              <span className="blog-post-date">16 April</span>
+            </div>
+          <h2>2020</h2>
+            <div>
+              <a classname="blog-post-title" href="/2020/09/08/rps-game">Rock Paper Scissors</a>
+              <span className="blog-post-date">8 September</span>
+            </div>
+        </div>
+      </body>
+      
     </div>
   )
 }
