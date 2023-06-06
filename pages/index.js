9a4abdff76b98bbc7ca071157cf097ecde823d44
Aussie Frost <65328557+austinfroste@users.noreply.github.com>
2023-06-05 20:45:02 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,10 +13,13 @@ export default function Home() {
       <body>
         <div>
 	  <p>Howdy, I'm <b>Aussie Frost</b>, a third year Data Science student working in UO's Computational Social Neuroscience Lab. 
-	  If you have any inquiries, or would just like to chat, feel free to email! You can also check out my projects on GitHub. ✌️</p>
+	  If you have any inquiries, or would just like to chat, feel free to email! You can also check out my projects on GitHub and connect with me on LinkedIn. ✌️</p>
 	</div>	
 	<div>
-	  <a href="mailto:afrost6@uoregon.edu">Email</a> <a href="https://github.com/austinfroste">GitHub</a> <a href="https://csnl.uoregon.edu/people/">CSNLab</a>
+	  <a href="mailto:afrost6@uoregon.edu">Email</a> 
+	  <a href="https://github.com/austinfroste">GitHub</a> 
+	  <a href="https://csnl.uoregon.edu/people">CSNLab</a> 
+	  <a href="https://www.linkedin.com/in/austinfroste">LinkedIn</a>
 	</div>
       </body>
     </div>
