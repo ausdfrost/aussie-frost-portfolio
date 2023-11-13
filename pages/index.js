8d18ad0afc4e5c3d6b5d99c4b7e8e3e7aae18da7
Aussie Frost <afrost6@uoregon.edu>
2023-11-12 20:46:33 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       <Header />
       <body>
         <div>
-	  <p>Howdy, I'm <b>Aussie Frost</b>, a rising fourth year Data Science student working in UO's <a href="https://csnl.uoregon.edu/people">Computational Social Neuroscience Lab</a>. 
+	  <p>Howdy, I'm <b>Aussie Frost</b>, a fourth year Data Science student and alum of UO's <a href="https://csnl.uoregon.edu/people">Computational Social Neuroscience Lab</a>. 
 	  	If you have any inquiries, or would just like to chat, feel free to <a href="mailto:afrost6@uoregon.edu">email</a>! 
 		You can also check out my projects on <a href="https://github.com/austinfroste">GitHub</a> and connect with me on <a href="https://www.linkedin.com/in/austinfroste">LinkedIn</a>. ✌️
 	  </p>
