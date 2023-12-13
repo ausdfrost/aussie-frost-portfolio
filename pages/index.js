92821e868c91f09c908b26e4508c3de5040462ff
Aussie Frost <afrost6@uoregon.edu>
2023-12-13 08:30:41 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
 	        <nav>
 	            <ul>
 	                <li><a href="#projects">Projects</a></li>
-	                <li><a href="#experience">Experience</a></li>
+	                <li><a href="https://www.linkedin.com/in/austinfroste/#experience">Experience</a></li>
 	                <li><a href="#about">About</a></li>
 	            </ul>
 	        </nav>
