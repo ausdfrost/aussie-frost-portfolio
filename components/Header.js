ad7cd45e6574f20f27604278b86838126a130e48
Aussie Frost <afrost6@uoregon.edu>
2023-12-13 16:34:28 -0800

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -4,14 +4,14 @@ export default function Header() {
   return (
     <>
       <header>
-	        <nav>
-	            <ul>
-	                <li><a href="#projects">Projects</a></li>
-	                <li><a href="https://www.linkedin.com/in/austinfroste/#experience">Experience</a></li>
-	                <li><a href="#about">About</a></li>
-	            </ul>
-	        </nav>
-	    </header>
+	<nav>
+	    <ul>
+		<li><a href="#projects">Projects</a></li>
+		<li><a href="https://www.linkedin.com/in/austinfroste/#experience">Experience</a></li>
+		<li><a href="mailto:afrost6@uoregon.edu">Contact</a></li>
+	    </ul>
+	</nav>
+      </header>
     </>
   )
 }
